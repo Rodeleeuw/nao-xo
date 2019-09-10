5823b48f02069059b58e632ae4f9f0161db25649
Ale <rodeleeuw@gmail.com>
2019-09-10 10:59:32 +0200

    first commit, forked project

---
+++ test_nao.py
@@ -0,0 +1,23 @@
+import sys
+from naoqi import ALProxy
+
+IP = "192.168.0.102"
+PORT = 9559
+
+try:
+    tts = ALProxy("ALTextToSpeech", IP, PORT)
+except Exception,e:
+    print "Could not create proxy to ALTextToSpeech"
+    print "Error was: ",e
+    sys.exit(1)
+
+
+print sys.version_info
+
+
+tts.setVolume(0.9)  ##Volume set to 90%
+tts.setParameter("pitchShift", 1.2) #Applies a pitch shifting to the voice
+tts.setParameter("doubleVoice", 0.0) #Deactivates double voice
+
+tts.say("Hiya. My name is NAO")
+tts.say("Bye-bye")
